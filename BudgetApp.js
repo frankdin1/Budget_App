f55d0b72fb08a4ae626371c6303d5d7debc05cf3
frankdin <frankdin@yahoo.com>
2020-10-21 18:36:10 -0500

    created function to return user input

--- BudgetApp.js
+++ BudgetApp.js
@@ -29,20 +29,34 @@ var firstController = (function(){
 
 var UIController = (function(){
 
+	return{
+		getInput: function(){//this is a public method that we have access to from outside
+			return {//this is an object that contains these 3 properties and thi object can be used as a parameter for the next function
+				type: document.querySelector('.add__type').value, //will either be inc (income) or exp (expense)
+				description: document.querySelector('.add__description').value,
+				value: document.querySelector('.add__value').value
+			}
+			
+		}
+	}
+
 })();
 
 var secondController = (function(budgetCtrl, UICtrl){
 	
 	var ctrlAddItem = function(){
-		/* 1. Get the field input data
-		   2. Add item to budget controller
-		   3. Add item to the UI
-		   4. Calculate budget
-		   5. Display budget on UI
-		*/
+		//1. Get the field input data
+		var input = UICtrl.getInput();
+		console.log(input);
+
+		//2. Add item to budget controller
+		//3. Add item to the UI
+		//4. Calculate budget
+		//5. Display budget on UI
 	}
 
 	document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+		ctrlAddItem();
 
 	document.addEventListener('keypress', function(event){
 
@@ -50,6 +64,6 @@ var secondController = (function(budgetCtrl, UICtrl){
 			ctrlAddItem();
 		}
 		// console.log(event);
-	})
+	});
 
 })(firstController, UIController);
