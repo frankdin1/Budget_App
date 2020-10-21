d8a3d46cab88e967ac8a129f73807fa06b981c66
frankdin <frankdin@yahoo.com>
2020-10-21 18:00:36 -0500

    created modules; created events for button click and ENTER button keypress

---
+++ BudgetApp.js
@@ -0,0 +1,55 @@
+//Module pattern example
+ // var firstController = (function(){
+ // 	var x = 23;
+
+ // 	var sum = function(a){
+ // 		return x + a;
+ // 	}
+
+ // 	return {
+ // 		publicTest: function(b){
+ // 			return sum(b);
+ // 		}
+ // 	}
+ // })();
+
+ // var secondController = (function(budgetCtrl){
+ // 	var z = budgetCtrl.publicTest(5);
+ // 		return {
+ // 		publicTest2: function(){
+ // 			console.log(z)
+ // 		}
+ // 	}
+ // })(firstController);
+
+//Budget Controller
+var firstController = (function(){
+ 
+})();
+
+var UIController = (function(){
+
+})();
+
+var secondController = (function(budgetCtrl, UICtrl){
+	
+	var ctrlAddItem = function(){
+		/* 1. Get the field input data
+		   2. Add item to budget controller
+		   3. Add item to the UI
+		   4. Calculate budget
+		   5. Display budget on UI
+		*/
+	}
+
+	document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+	document.addEventListener('keypress', function(event){
+
+		if (event.keyCode === 13 || event.which === 13){//keyCode works on recent browsers while 'which' works on older browsers
+			ctrlAddItem();
+		}
+		// console.log(event);
+	})
+
+})(firstController, UIController);
