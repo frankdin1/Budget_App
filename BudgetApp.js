1f4b8a0253708ade6e25f7a65aca7ddbf13b2250
frankdin <frankdin@yahoo.com>
2020-10-21 19:25:16 -0500

    created object for class variables and made the object public

--- BudgetApp.js
+++ BudgetApp.js
@@ -24,19 +24,34 @@
 
 //Budget Controller
 var firstController = (function(){
+
+	
  
 })();
 
 var UIController = (function(){
 
+	//this is an object for all inputs
+	var DOMStrings = {
+		inputType: '.add__type',
+		inputDescription: '.add__description',
+		inputValue: '.add__value',
+		inputButton: '.add__btn'
+	}
+
 	return{
 		getInput: function(){//this is a public method that we have access to from outside
 			return {//this is an object that contains these 3 properties and thi object can be used as a parameter for the next function
-				type: document.querySelector('.add__type').value, //will either be inc (income) or exp (expense)
-				description: document.querySelector('.add__description').value,
-				value: document.querySelector('.add__value').value
+				type: document.querySelector(DOMStrings.inputType).value, //will either be inc (income) or exp (expense)
+				description: document.querySelector(DOMStrings.inputDescription).value,
+				value: document.querySelector(DOMStrings.inputValue).value
 			}
 			
+		},
+
+		//this is an object to make the inputs object public
+		getDOMStrings: function(){
+			return DOMStrings;
 		}
 	}
 
@@ -44,6 +59,8 @@ var UIController = (function(){
 
 var secondController = (function(budgetCtrl, UICtrl){
 	
+	var DOM = UICtrl.getDOMStrings();//this adds the DOMstrings object to this private function
+
 	var ctrlAddItem = function(){
 		//1. Get the field input data
 		var input = UICtrl.getInput();
@@ -51,11 +68,16 @@ var secondController = (function(budgetCtrl, UICtrl){
 
 		//2. Add item to budget controller
 		//3. Add item to the UI
+		// if(input.type === "inc"){
+		// 	document.querySelector('.item__description').innerHTML = input.description;
+		// 	document.querySelector('.item__description').innerHTML = input.description;
+		// }
+		// console.log(input.description);
 		//4. Calculate budget
 		//5. Display budget on UI
 	}
 
-	document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+	document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
 		ctrlAddItem();
 
 	document.addEventListener('keypress', function(event){
